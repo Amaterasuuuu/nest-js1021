05cb3ac93ea25acc3545be8771a867dd21bb2a92
amaterasuuuu <foreverwakandaa@gmail.com>
2022-01-27 20:28:16 +0600

    feature: added validation and cleaned code

--- src/image/dto/create-image.dto.ts
+++ src/image/dto/create-image.dto.ts
@@ -1,6 +1,19 @@
+import { Contains, IsEnum, IsOptional, Length } from 'class-validator';
+import { Category } from '../enum';
+
 export class CreateImageDto {
-  id?: number
-  name: string
-  url: string
-  description: string
+  id?: number;
+
+  @Length(2, 30)
+  name: string;
+
+  @IsEnum(Category)
+  category: Category;
+
+  @Contains('https://')
+  url: string;
+
+  @IsOptional()
+  @Length(0, 100)
+  description: string;
 }
--- src/image/image.service.ts
+++ src/image/image.service.ts
@@ -1,51 +1,73 @@
-import { ConflictException, Injectable, NotFoundException } from '@nestjs/common';
-import { CreateImageDto } from './dto/create-image.dto';
-import { UpdateImageDto } from './dto/update-image.dto';
-import { IImage } from './interface/image.interface';
+import {
+  ConflictException,
+  Injectable,
+  NotFoundException,
+} from '@nestjs/common';
+import { CreateImageDto, UpdateImageDto } from './dto';
+import { Category } from './enum';
+import { IImage } from './interface';
 
 @Injectable()
 export class ImageService {
-  private DB: IImage[]
+  private DB: IImage[];
   constructor() {
     this.DB = [
-      { id: 1, name: 'Hatiko', url: 'https://bit.ly/3fF431Z', description: '' },
-      { id: 2, name: 'My hero academia', url: 'https://bit.ly/3Ae5HRK', description: '' },
-      { id: 3, name: 'Macbook air M1', url: 'https://bit.ly/3IqLCKV', description: '' },
-    ]
+      {
+        id: 1,
+        name: 'Hatiko',
+        category: Category.ANIMALS,
+        url: 'https://bit.ly/3fF431Z',
+        description: '',
+      },
+      {
+        id: 2,
+        name: 'MHA',
+        category: Category.ANIME,
+        url: 'https://bit.ly/3Ae5HRK',
+        description: '',
+      },
+      {
+        id: 3,
+        name: 'Macbook air M1',
+        category: Category.TECH,
+        url: 'https://bit.ly/3IqLCKV',
+        description: '',
+      },
+    ];
   }
 
   async findAll(): Promise<IImage[]> {
-    return this.DB
+    return this.DB;
   }
 
   async findOne(id: number): Promise<IImage> {
-    const image = this.DB.find((image: IImage) => image.id === id)
+    const image = this.DB.find((image: IImage) => image.id === id);
     if (!image) {
-      throw new NotFoundException('Image is not defined')
+      throw new NotFoundException('Image is not defined');
       // throw new HttpException('Ты пес, ты что тут отправялешь', HttpStatus.NOT_FOUND)
     }
-    return image
+    return image;
   }
 
   async create(dto: CreateImageDto): Promise<IImage> {
-    const exist = this.DB.find(image => image.url === dto.url)
+    const exist = this.DB.find((image) => image.url === dto.url);
     if (exist) {
-      throw new ConflictException('Image already exist')
+      throw new ConflictException('Image already exist');
     }
-    dto.id = this.DB.length + 1
-    this.DB.push(dto)
-    return dto
+    dto.id = this.DB.length + 1;
+    this.DB.push(dto);
+    return dto;
   }
 
   async update(id: number, dto: UpdateImageDto): Promise<IImage> {
-    const image = await this.findOne(id)
-    Object.assign(image, dto)
-    return image
+    const image = await this.findOne(id);
+    Object.assign(image, dto);
+    return image;
   }
 
   async delete(id: number): Promise<void> {
-    const image = await this.findOne(id)
-    const imageIndex = this.DB.indexOf(image)
-    this.DB.splice(imageIndex, 1)
+    const image = await this.findOne(id);
+    const imageIndex = this.DB.indexOf(image);
+    this.DB.splice(imageIndex, 1);
   }
 }
